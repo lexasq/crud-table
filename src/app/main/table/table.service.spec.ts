276acfb9a34096808231fd235ce5adcb45ee5c6e
Alexey Umanskiy <sharkakatsukie@gmail.com>
2022-11-21 21:34:13 +0200

    Update table.service.spec.ts

--- src/app/main/table/table.service.spec.ts
+++ src/app/main/table/table.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { TableService } from './table.service';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('TableService', () => {
   let service: TableService;
